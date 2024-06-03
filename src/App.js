9cf46c44c4f4cdf2ed9a137468cea982bc688ab5
huuthanh0312 <huuthanhnguyen0312@gmail.com>
2024-06-03 14:44:15 +0700

    Render element

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Thanh';
+  const age = 24;
+  const isMale = true;
+  const student = {
+    name: 'Thanh Frontend'
+  };
+  const colorList = ['red', 'green', 'yellow', 'blue']
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Thanh Nguyen 123
         </p>
-        
-        Thanh Nguyen
-        
+        <p>Xinchao {name} - Age: {age} {isMale ? 'Male' : 'Female'}</p>
+        {isMale && <p>Male</p>}
+        {!isMale && <p>Female</p>}
+        {isMale && (<>
+          <p>Male</p>
+          <p>Male</p>
+          <p>Male</p>
+        </>)}
+        <p>{student.name}</p>
+        <ul>
+          {colorList.map( color => (
+          <li>{color}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
